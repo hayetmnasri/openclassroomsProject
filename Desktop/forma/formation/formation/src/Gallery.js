f2b2ff14d6b5147d6481c20fa159f0f8cabe4ad6
hayetmnasri <hayetmnasri@hotmail.fr>
2023-11-28 20:04:04 +0100

    Convertir du HTML en JSX

--- Desktop/forma/formation/formation/src/Gallery.js
+++ Desktop/forma/formation/formation/src/Gallery.js
@@ -1,11 +1,4 @@
-export function Profile() {
-    return (
-      <img
-        src="https://i.imgur.com/QIrZWGIs.jpg"
-        alt="Alan L. Hart"
-      />
-    );
-  }
+
   
   export default function Gallery() {
     return (
